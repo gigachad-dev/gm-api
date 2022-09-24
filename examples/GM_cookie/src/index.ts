5238fec374f92ae770cd543bab2346f0854c5483
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-09-24 10:16:04 +0800

    feat(GM_cookie): deserialize, serialize

--- examples/GM_cookie/src/index.ts
+++ examples/GM_cookie/src/index.ts
@@ -1,20 +1,42 @@
 import { GM_cookie } from '@gm-api/cookie'
 
-const expires = new Date()
 // 7 days
-expires.setDate(expires.getTime() + 7 * 24 * 60 * 60 * 1000)
+const expires = new Date()
+expires.setTime(expires.getTime() + 7 * 24 * 60 * 60 * 1000)
 
-GM_cookie.set('john', JSON.stringify({ id: 1, name: 'John' }), { expires })
+// GM_cookie.set('john', JSON.stringify({ id: 1, name: 'John' }), { expires })
 
-GM_cookie.set('sam', JSON.stringify({ id: 2, name: 'Sam' }), { expires })
+// GM_cookie.set('sam', JSON.stringify({ id: 2, name: 'Sam' }), { expires })
 
-type Cookies = {
-  john: string
-  sam: string
+// type Cookies = {
+//   john: string
+//   sam: string
+// }
+
+// const cookies = GM_cookie.list<Cookies>()
+// console.log({ cookies, john: JSON.parse(cookies.john) })
+
+// GM_cookie.delete('sam')
+// console.log(GM_cookie.list())
+
+const cookie = new GM_cookie({
+  serialize(value) {
+    return JSON.stringify(value)
+  },
+  deserialize(value) {
+    try {
+      return JSON.parse(value)
+    } catch (err) {
+      return null
+    }
+  }
+})
+
+interface User {
+  id: number
+  name: string
 }
 
-const cookies = GM_cookie.list<Cookies>()
-console.log({ cookies, john: JSON.parse(cookies.john) })
-
-GM_cookie.delete('sam')
-console.log(GM_cookie.list())
+console.log(cookie.get<User>('john'))
+cookie.set('le_xot', { id: 3, name: 'Lesha' }, { expires })
+console.log(cookie.get<User>('le_xot'))
